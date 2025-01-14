30df9d177338ab135e5af65816f73b48997e4860
amanbhartiiii <amankumarbharti824112@gmail.com>
2025-01-14 15:27:20 +0530

    Initial commit

---
+++ src/test/java/com/bharti/Streaming_app/StreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharti.Streaming_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
